2bd862d831e8d1dc0fdba20ba003cc532f7d9809
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-11-27 00:46:01 +0800

    Added notes on how to dump preprocessor output and a sample file.

--- cpp/refactor/tetris/main.cpp
+++ cpp/refactor/tetris/main.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+/**
+ * To verify preprocessing, try:
+ * - `g++ -I. -E -P ./main.cpp > ./dump.cpp`
+ * 1. `-I` means uses the specified directory for headers. and `-I.` means include the current directory.
+ * 2. `-E` means stop at the preprocessing stage. By default we output to stdout.
+ * 3. `-P` means omit linemarkers from the preprocessor output.
+ * 
+ * @note See: https://stackoverflow.com/a/30033131, https://gcc.gnu.org/onlinedocs/cpp/Invocation.html#index-P
+*/
 int main() {
     Tetris tetris = Tetris();
     bool is_gameover = false;
